35023992de23569156b19dc8a19ad59988fa6be7
James <admin@James.local>
2021-03-25 00:50:01 +0300

    menu content

---
+++ src/containers/Menu.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "../assets/styles/menu.css";
+
+const Menu = ({ appMode }) => {
+  const appMenu = [
+    { id: 1, link: "Entry1" },
+    { id: 2, link: "Entry2" },
+    { id: 3, link: "Entry3" },
+    { id: 4, link: "Entry4" },
+    { id: 5, link: "Entry5" },
+  ];
+  const adminMenu = [
+    { id: 1, link: "AdminEntry1" },
+    { id: 2, link: "AdminEntry2" },
+    { id: 3, link: "AdminEntry3" },
+  ];
+  const [menuItems, setMenuItems] = useState([]);
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    if (appMode == "admin") {
+      setMenuItems(adminMenu);
+    } else if (appMode == "app") {
+      setMenuItems(appMenu);
+    }
+  });
+  console.log("selected", selected);
+  return (
+    <div>
+      {menuItems.map((menu) => (
+        <div
+          onClick={() => setSelected(menu.id)}
+          className={`${selected == menu.id ? "menu-active" : "menu"}`}
+          key={menu.id}
+        >
+          <p>{menu.link}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
